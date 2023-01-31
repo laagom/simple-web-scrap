c2b9be65bfbfe822f5f27f1a381bc591e56c72f1
laagom <cjyooong@gmail.com>
2023-01-31 14:48:16 +0900

    23-01-31 [web scrapping] [selenium 적용]

--- README.md
+++ README.md
@@ -2,12 +2,18 @@
 
 ## Goal
 여러가지 사이트를 스크랩해서 스크랩 내용을 엑셀로 정리할 수 있는 기능 구현
+
+<br>
+
 ## List
 
 - [x] 직업 공고사이트 
 
   > [weworkremotely](https://weworkremotely.com/)를 스크랩하여 공고 정보를 정리
-  >
+
+  > [indeed](https://kr.indeed.com/?from=gnav-jobsearch--indeedmobile)를 스크랩하여 공고 정보 정리
+
+<br>
 
 ## Pakages
  > - requests==2.28.2<br>
@@ -15,3 +21,19 @@
  
  > - beautifulsoup4==4.11.1<br>
  >  : 응답받은 웹사이트의 HTML을 키워드를 사용하여 데이터 가공을 위한 패키지
+
+ > - selenium==4.8.0<br>
+ >  : 브라우저에 동적이 자동화 기능을 넣기 위해 beautifulsoup 대체제
+ 
+ > - chromedriver
+ >  : selenium으로 크롭에 접근하기 위한 드라이버
+
+ > - webdriver-manager==3.8.5
+ >  : 크롭드라이버를 최신 버전으로 유지 하기 위한 패키지 
+
+<br>
+
+ ## Issue
+> ***[23-01-31] Indeed 사이트를 BeatifulSoup으로 접근 시 bot으로 판단하여 접근 차단*** <br>
+> 
+> - 해결방법 : 동적으로 접근할 수 있는 기능이 있는 Selenium을 사용하여 브라우저 접근 후 Indeed로 접속할 수 있게 개선
--- scrap/extractors/idd.py
+++ scrap/extractors/idd.py
@@ -1,29 +1,33 @@
 from requests import get
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 def idd_jobs(keyword):
-    # indeed 사이트 검색 url 
-    base_url = 'https://kr.indeed.com/jobs?q='
+    # Issue Indeed 403 Fix
+    # ------------------------------------------------------
+    # 내용    : 초기 indeed사이트 접근시 bot인지 구분 없이 접근 허용
+    #          현재 bot으로 접근하는 경우 차단되어 403 상태 코드 발생
+    # ------------------------------------------------------
+    # 해결방법 : BeautifulSoup 대체제로 Selenuium 사용
+    #        : Selenium은 코드를 사용해서 브라우저를 자동화 할 수 있음
+    #        : 크롬으로 접근 후 indeed로 접근시켜 indeed가 bot으로 인지하지 못하게 함
 
-    # 요청 후 응답 받기
-    response = get(f"{base_url}{keyword}")
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True) # 브라우저 꺼짐 방지 코드
 
-    # 응답 상태 코드에 따른 분기 처리
-    if response.status_code != 200:
-        print("Cant request pagge")
-    else:
-        result = []
+    # 크롬드라이버를 최신으로 유지해줍니다.
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options = chrome_options) 
+    
+    browser.get("https://kr.indeed.com/jobs?q=python")
 
-        soup = BeautifulSoup(response.text, "html.parse")
-        jobs_list = soup.find_all('ul', class_='jobsearch-ResultsList')
-        
-        # 현재 depth의 li요소만 찾기 위해 recursive=False
-        jobs = jobs_list.find_all('li', recursive=False)
-        
-        for job in jobs:
-            print(job)
-            print('--------------------')
+    
 
-        return response.text
+    
+
+
+
 
 
